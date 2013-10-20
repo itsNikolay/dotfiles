29f4611ea5f1cec13de579250a7f1f0da13a9fe5
Ponomarev Nikolay <itsnikolay@gmail.com>
2013-10-20 22:40:08 +0400

    adding other instalations

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 # chmod u+x install.sh
 rm ~/.gitconfig
 ln -s `pwd`/.gitconfig ~/.gitconfig
+
+rm ~/.bash_aliases
+ln -s `pwd`/.bash_aliases ~/.bash_aliases
+
+rm ~/.vimrc
+ln -s `pwd`/.vimrc ~/.vimrc
