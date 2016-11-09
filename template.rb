89e6f600673df97421f25a25b2cb8fe591891911
Nikolay Ponomarev <itsnikolay@gmail.com>
2016-11-10 01:28:43 +0300

    Add conditions

--- template.rb
+++ template.rb
@@ -1,34 +1,46 @@
-git :init
-git add: '.'
-git commit: 'initial'
-
-gem_group :test do
-  gem 'webmock'
-  gem 'vcr'
+if yes? 'Do initial commit?'
+  git :init
+  git add: '.'
+  git commit: 'initial'
 end
 
-gem_group :development, :test do
-  gem 'pry-rails'
-  gem 'pry-byebug'
-  gem 'rspec-rails'
-  gem 'factory_girl_rails'
+if yes? 'Add Rspec?'
+  gem_group :test do
+    gem 'webmock'
+    gem 'vcr'
+  end
+  gem_group :development, :test do
+    gem 'rspec-rails'
+    gem 'factory_girl_rails'
+  end
+  gem_group :development do
+    gem 'spring'
+    gem 'spring-commands-rspec'
+  end
+  run :bundle
+  run spring: 'binstub --all'
+  generate 'rspec:install'
 end
 
-gem_group :development do
-  gem 'spring'
-  gem 'spring-commands-rspec'
-  gem 'guard'
-  gem 'guard-rspec'
-  gem 'guard-bundler'
+if yes? 'Add Pry?'
+  gem_group :development, :test do
+    gem 'pry-rails'
+    gem 'pry-byebug'
+  end
+  run :bundle
 end
 
-run :bundle
-run spring: 'binstub --all'
-run bundle: 'exec guard init'
-run bundle: 'exec guard init rspec'
-run bundle: 'exec guard init bundler'
-
-generate 'rspec:install'
+if yes? 'Add Guard?'
+  gem_group :development do
+    gem 'guard'
+    gem 'guard-rspec'
+    gem 'guard-bundler'
+  end
+  run :bundle
+  run bundle: 'exec guard init'
+  run bundle: 'exec guard init rspec'
+  run bundle: 'exec guard init bundler'
+end
 
 git :init
 git add: '.'
