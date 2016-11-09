5b3cb0d191eb0bac82da5716cb028e1c60206a1b
Nikolay Ponomarev <itsnikolay@gmail.com>
2016-11-10 01:03:30 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for i in $(ls -Ad .* | grep ".[a-zA-Z0-9]"); do
+# Install dotfiles
+
+for i in $(ls -Ad .* | grep ".[^git$][a-zA-Z0-9]"); do
   echo $i;
+  command="ln -sf $PWD/$i $HOME/$i";
+  echo $command;
+  $command;
 done
